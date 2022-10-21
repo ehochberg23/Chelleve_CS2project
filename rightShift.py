54bd6631dbb081ef2a15d26c4538c53b60ec8b8c
spaul24-beep <115804074+spaul24-beep@users.noreply.github.com>
2022-10-21 11:47:03 -0400

    Added rightShift
    Added broken code

---
+++ rightShift.py
@@ -0,0 +1,28 @@
+from _operator import length_hint
+
+def rightShift(text,num ):   
+    
+   
+    
+    end= len(text)
+    end = end-3
+    word = text[0:end]
+    
+    print(word) 
+    
+    cnt = 0
+    while cnt <length_hint
+    data= "#" + data 
+    
+    
+def main():
+    
+    
+    
+    rightShift("COMPUTER", 3)
+    
+    
+                
+if __name__ == '__main__':
+    main() 
+    
