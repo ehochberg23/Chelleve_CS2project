b75e8f2726ce1a2b7eef41bd213b9d982fa38f20
ehochberg23 <ehochberg23@gcds.net>
2022-10-26 11:26:44 -0400

    ellie final?

--- leftCirc.py
+++ leftCirc.py
@@ -3,11 +3,11 @@ def leftCirc(num, text):
     circ = text[num:]
     text = text[:num]
     text = circ + text
-    return text
+    print(text)
 
 
 def main():
-    
+    data = "LC-3/COMPUTER"
     data = data.split("/")
     data_num = len(data)
     
@@ -19,35 +19,13 @@ def main():
         command = command.split("-")
         function = command[0]
         
-        if function == "LS":
-            num = int(command[-1])
-            text = leftShift(num, text)
+
             
-        elif function == "RS":
-            num = int(command[-1])
-            text = rightShift(num, text)
-            
-        elif function == "LC":
+        if function == "LC":
             num = int(command[-1])
             text = leftCirc(num, text)
             
-        elif function == "RC":
-            num = int(command[-1])
-            text = rightCirc(num, text) 
-            
-        elif function == "MC":
-            func_commands = command[-1]
-            position = int(func_commands[-1])
-            length = int(func_commands[-2])
-            num = int(func_commands[-3])
-            direction = str(func_commands[-4])
-            text = subCirc(position, length, num, direction, text)
-        
-        elif function == "REV":
-            func_commands = command[-1]
-            position = int(func_commands[-1])
-            length = int(func_commands[-2])
-            text = rev(position, length, text)
+
               
         count = count + 1
         
--- rightShift.py
+++ rightShift.py
@@ -1,23 +1,41 @@
-from _operator import length_hint
 
-def rightShift(text,num ):   
-    
-   
-    
+def rightShift(text,num):
+
     end= len(text)
-    end = end-3
+    end = end-num
     word = text[0:end]
     
     print(word) 
     
     cnt = 0
-    while cnt <length_hint
-    data= "#" + data 
+    while cnt < length_hint
+    data = "#" + data
     
     
 def main():
-    
-   
-    
+
+    data = "RS-1/OHIO"
+
+    data = data.split("/")
+    data_num = len(data)
+
+    text = data[-1]
+
+    count = 0
+    while count < data_num:
+        command = data[count]
+        command = command.split("-")
+        function = command[0]
+
+        if function == "RS":
+            num = int(command[-1])
+            text = rightShift(num, text)
+
+
+        count = count + 1
+
+    return text
+
+
 if __name__ == "__main__":
     main()
