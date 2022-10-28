97b2e388f000cd47cc2e441723d8dcf896df9bf5
spaul24-beep <115804074+spaul24-beep@users.noreply.github.com>
2022-10-28 12:42:56 -0400

    Reverse String
    This is my reverse string

---
+++ reverseString.py
@@ -0,0 +1,36 @@
+
+def main():
+    
+    out = reverseString(3,3, "COMPUTER")
+    
+    print(out)
+
+
+def reverseString(position, length, text):
+     """
+    Sum up two integers
+    Arguments:
+        text: a string
+        length: an integer
+        position: an integer
+    Returns:
+        A string which is reversed of the part 
+    """
+    output = "" #
+    string = text
+    before_reverse = text[:position-1]#Keeping letters before the reverse letters
+    after_reverse = text[position + length -1:]#Keeping letters after the reverse letters the same
+    for i in range(len(string)-1, -1, -1):#Reversing the middle letters
+        output = output + string[i]
+    end = position + length
+    output = output[position:end]
+    
+    
+    return before_reverse + output + after_reverse
+    
+
+
+
+
+
+    
--- rightShift.py
+++ rightShift.py
@@ -1,48 +1,21 @@
-def rightShift(num, text):
-    # rightShift -
-    # text - the word
-    # num -number of letters you are removing from the word
 
 
-    cnt = 0  # the counter for the while loop
-    end = len(text)
-    end = end - num
-    word = text[0:end]
+def rightShift(text, num ):   
+    #rightShift - 
+    #text - the word
+    #num -number of letters you are removing from the word
+   
+    cnt = 0                                             #the counter for the while loop
+    end= len(text)                                      
+    end = end-num                                       
+    word = text[0:end]                                  
+    
+    while cnt < num:                                    #Determining how many hashtags it needs to add to the front so it runs the loop that many times
+        
+        word = "#" + word                               #Adds the hashtags that removed the letters to the beginning of the word
+        cnt +=1
+    
+    return word                                         #Returns the finished word
+    
 
-    while cnt < num:  # Determining how many hashtags it needs to add to the front so it runs the loop that many times
-
-        word = "#" + word  # Adds the hashtags that removed the letters to the beginning of the word
-        cnt += 1
-
-    print(word)  # Returns the finished word
-
-
-def main():
-
-    data = "RS-3/COMPUTER"
-
-    data = data.split("/")
-    data_num = len(data)
-
-    text = data[-1]
-
-    count = 0
-    while count < data_num:
-        command = data[count]
-        command = command.split("-")
-        function = command[0]
-
-        if function == "RS":
-            num = int(command[-1])
-            text = rightShift(num, text)
-
-
-        count = count + 1
-
-    return text
-
-
-if __name__ == "__main__":
-    main()
-
-
+    
