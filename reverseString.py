81188ca9eff592b2fed05fb4e38c67b23f1f4892
ehochberg23 <ehochberg23@gcds.net>
2022-10-31 11:18:21 -0400

    reverseString_final

--- reverseString.py
+++ reverseString.py
@@ -9,26 +9,25 @@ def reverseString(position, length, text):
     """
     This function reverses the middle letters based on position and the length
     Arguments:
-        text: a string
-        length: an integer
-        position: an integer
+        position: starting position
+        length: length of string being reversed
+        text: the string that contains the word being manipulated
     Returns:
-        A string which is reversed of the part 
+        text: A string which is reversed of the part
     """
-    output = "" #
+    output = ""
     string = text
-    before_reverse = text[:position-1]#Keeping letters before the reverse letters
-    after_reverse = text[position + length -1:]#Keeping letters after the reverse letters the same
-    for i in range(len(string)-1, -1, -1):#Reversing the middle letters
+    before_reverse = text[:position - 1]  # Keeping letters before the reverse letters
+    after_reverse = text[position + length - 1:]  # Keeping letters after the reverse letters the same
+    for i in range(len(string) - 1, -1, -1):  # Reversing the middle letters
         output = output + string[i]
+    position = position + 1
     end = position + length
     output = output[position:end]
-    
-    
-    return before_reverse + output + after_reverse
-    
 
+    text = before_reverse + output + after_reverse
 
+    return text
 
 
 
