cc78202ec4d72337ed3318923efeb7809018115f
ehochberg23 <ehochberg23@gcds.net>
2022-10-31 11:20:45 -0400

    rightShift_final

--- rightShift.py
+++ rightShift.py
@@ -1,21 +1,25 @@
 
 
-def rightShift(text, num ):   
-    #rightShift - 
-    #text - the word
-    #num -number of letters you are removing from the word
-   
-    cnt = 0                                             #the counter for the while loop
-    end= len(text)                                      
-    end = end-num                                       
-    word = text[0:end]                                  
-    
-    while cnt < num:                                    #Determining how many hashtags it needs to add to the front so it runs the loop that many times
-        
-        word = "#" + word                               #Adds the hashtags that removed the letters to the beginning of the word
-        cnt +=1
-    
-    return word                                         #Returns the finished word
-    
+def rightShift(num, text):
+    """
+    Shifts all the characters of the string 'num' places to the right. Delete the rightmost 'num' characters and replace with #'s
+    Arguments:
+        num: the number of places the string will be shifted to the right
+        text: the string being manipulated
+    Returns:
+        word: the resulting string
+    """
 
-    
+    cnt = 0  # the counter for the while loop
+    end = len(text)
+    end = end - num
+    word = text[0:end]
+
+    while cnt < num:  # Determining how many hashtags it needs to add to the front so it runs the loop that many times
+
+        word = "#" + word  # Adds the hashtags that removed the letters to the beginning of the word
+        cnt += 1
+        text = word
+    return text  # Returns the finished word
+
+
